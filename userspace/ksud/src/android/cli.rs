da3903d3bb4e9a22a8792f6e495803ac76f2300f
Wang Han <416810799@qq.com>
2026-09-06 12:59:35 +0800

    feat(ksud): Implement SIGSYS handler (https://github.com/tiann/KernelSU/pull/3677)
    [cherry-picked upstream commit https://github.com/tiann/KernelSU/commit/35feb58872b6f88ac1b488ce46a68892a017d62d]
    
    Co-Authored-By: 5ec1cff <ewtqyqyewtqyqy@gmail.com>
    Co-Authored-By: AlexLiuDev233 <wzylin11@outlook.com>

--- userspace/ksud/src/android/cli.rs
+++ userspace/ksud/src/android/cli.rs
@@ -572,6 +572,8 @@ pub fn run() -> Result<()> {
             .with_tag("KernelSU"),
     );
 
+    ksucalls::setup_sigsys_handler();
+
     // the kernel executes su with argv[0] = "su" and replace it with us
     let arg0 = std::env::args().next().unwrap_or_default();
     if arg0 == "su" || arg0.ends_with("/su") {
@@ -871,7 +873,7 @@ pub fn run() -> Result<()> {
             Kernel::Umount { command } => match command {
                 UmountOp::Add { mnt, flags } => ksucalls::umount_list_add(&mnt, flags),
                 UmountOp::Del { mnt } => ksucalls::umount_list_del(&mnt),
-                UmountOp::Wipe => ksucalls::umount_list_wipe().map_err(Into::into),
+                UmountOp::Wipe => ksucalls::umount_list_wipe(),
                 UmountOp::List => {
                     let list = ksucalls::umount_list_list()?;
                     println!("{}", serde_json::to_string(&list)?);
